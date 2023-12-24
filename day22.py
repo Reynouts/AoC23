1cae9fda5b50653b18a03fabc8e6cb602291067f
Reinout <rcw.versteeg@avans.nl>
2023-12-24 23:21:01 +0100

    Tetris

--- day22.py
+++ day22.py
@@ -1,87 +1,43 @@
 import re
-from copy import deepcopy
-
-def get_neighbours(c, cells):
-    dirs = ((1, 0), (-1, 0), (0, 1), (0, -1))
-    return [pos for dx, dy in dirs if (pos := (c[0] + dx, c[1] + dy)) in cells]
+from collections import defaultdict
 
 
-class block:
-    blocks = set()
-    def __init__(self, sx, sy, sz, ex, ey, ez):
-        self.tiles = []
-        for z in range(sz+1, sz-1, -1):
-            for y in range(sy, ey+1):
-                for x in range(sx, sx+1):
-                    self.tiles.append([x, y, z])
+def intersects(b1, b2):
+    return max(b1[0], b2[0]) <= min(b1[3], b2[3]) \
+            and max(b1[1], b2[1]) <= min(b1[4], b2[4])
 
-    def __gt__(self, other):
-        return min([tile[2] for tile in self.tiles]) < min([tile[2] for tile in other.tiles])
 
+def checkfall(supporting, supported, n, falling):
+    if n in falling:
+        return 0
+    falling.add(n)
+    return sum(1 + checkfall(supporting, supported, i, falling)
+               for i in supporting[n] if not supported[i] - falling)
 
 
 def main():
     with open("day22.txt", 'r') as f:
-        instructions = f.read().split('\n')
+        blocks = [list(map(int, re.findall("\d+", line))) for line in f.read().split('\n')]
+    blocks.sort(key=lambda b: b[2])
 
-    blocks = []
-    occupied = set()
-    total = 0
-    for i in instructions:
-        blocks.append([])
-        sx, sy, sz, ex, ey, ez = map(int, re.findall("\d+", i))
-        for z in range(ez, sz-1, -1):
-            for y in range(sy, ey+1):
-                for x in range(sx, ex+1):
-                    blocks[-1].append((x, y, z))
-                    total += 1
-                    occupied.add((x, y, z))
-    print(total, len(occupied))
-    print(blocks)
-    down = True
-    while down:
-        down = False
-        for i, b in enumerate(blocks):
-            can_fall = True
-            for c in b:
-                if (c[0], c[1], c[2]-1) in (occupied - set(b)) or c[2]-1 == 0:
-                    can_fall = False
-            if can_fall:
-                down = True
-                new_block = []
-                for c in b:
-                    occupied.remove(c)
-                for c in b:
-                    new_block.append((c[0], c[1], c[2]-1))
-                    occupied.add((c[0], c[1], c[2]-1))
-                blocks[i] = new_block
-    mx = 0
-    for b in blocks:
-        for c in b:
-            if c[2] > mx:
-                mx = c[2]
-    print(mx)
+    for i, b1 in enumerate(blocks):
+        z = 1
+        for b2 in blocks[:i]:
+            if intersects(b1, b2):
+                z = max(z, b2[5] + 1)
+        b1[5] += z - b1[2]
+        b1[2] = z
 
-    cant = []
-    for b in blocks:
-        copycied = deepcopy(occupied)
-        for c in b:
-            copycied.remove(c)
-        for b2 in blocks:
-            if b2 != b:
-                can_fall = True
-                for c in b2:
-                    if (c[0], c[1], c[2] - 1) in (copycied - set(b2)) or c[2] - 1 == 0:
-                        can_fall = False
-                if can_fall:
-                    cant.append(b)
-                    #break
-                print(b, b2, can_fall)
+    supporting = defaultdict(set)
+    supported = defaultdict(set)
+    for j, b1 in enumerate(blocks):
+        for i, b2 in enumerate(blocks[:j]):
+            if intersects(b1, b2) and b1[2] == b2[5] + 1:
+                supporting[i].add(j)
+                supported[j].add(i)
+    print("p1:", sum([1 for i, b in enumerate(blocks) if all(len(supported[j]) >= 2 for j in supporting[i])]))
+    print("p2:", sum([checkfall(supporting, supported, i, set()) for i, _ in enumerate(blocks)]))
 
-        print("L:",len(cant))
-    print(len(blocks), len(cant))
-    print(len(blocks)-len(cant))
-    print(len(cant))
 
 if __name__ == "__main__":
     main()
